1f05544b20e28918801fb0ff10fd593a7468b3c5
jaRue1 <jaruejohnson@outlook.com>
2021-08-07 15:10:17 -0400

    added a reusable function to excute queries

--- mysql.js
+++ mysql.js
@@ -15,31 +15,20 @@ db.connect((err) => {
   console.log("Connection Established")
 
   query = "DROP TABLE IF EXISTS ANIME" // DROP TABLE CMD
-  db.query(query, (err) => {
-    // executes query
-    if (err) throw err
-    console.log("Anime Table was dropped")
-  })
+  executeQuery(query, "Anime Table was dropped")
 
   // created a table with specific columns
   query = "CREATE TABLE ANIME (row_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), " 
     + "anime_id INT, type VARCHAR(255), episodes INT, rating DECIMAL, members INT)"
-
-  db.query(query, (err) => {
-    // executes query
-    if (err) throw err
-    console.log("Anime table created.")
-  })
+  
+  executeQuery(query, "Anime table created.")
 
   // import the animeTable csv file into SQL DB / while ignoring the the first line of the file (headers)
   query = "LOAD DATA LOCAL INFILE 'csv/animeTable.csv' INTO TABLE ANIME FIELDS TERMINATED BY ',' IGNORE 1 LINES"
    +  "(name,anime_id,type,episodes,rating,members)" // order in which to put the columns
-
-  db.query(query, (err) => {
-    // executes query
-    if (err) throw err
-    console.log("Anime Table Loaded.")
-  })
+ 
+   executeQuery(query, "Anime Table Loaded.")
+ 
 
   db.end( err => {
     // closing connection to mysql DB
@@ -47,3 +36,10 @@ db.connect((err) => {
     console.log("All Done closing connection")
   })
 })
+// reusable function that executes SQL queries 
+function executeQuery(query,msg){
+  db.query(query, (err) =>{
+    if (err) throw err
+  })
+  console.log(msg)
+}
