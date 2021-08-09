198121968963b8a9e0a8f2fe22d96dd34f2b53cc
jaRue1 <jaruejohnson@outlook.com>
2021-08-08 22:52:34 -0400

    added an api endpoint that resets the ANIME_GENRE table

--- api.js
+++ api.js
@@ -35,21 +35,29 @@ app.get('/deleteAnime/:id', (req,res) => {// get api point that deletes a row fr
 })
 
 app.get('/resetAnime',(req,res) => {
-  const query = "DROP TABLE IF EXISTS Anime;"+
-    "CREATE TABLE Anime (row_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), anime_id INT, type VARCHAR(255), episodes INT, rating DECIMAL, members INT);" +
-    "LOAD DATA LOCAL INFILE 'csv/animeTable.csv' INTO TABLE Anime FIELDS TERMINATED BY ',' IGNORE 1 LINES (name, anime_id, type, episodes, rating, members);"
+  const query = "DROP TABLE IF EXISTS ANIME;"+
+    "CREATE TABLE ANIME (row_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), anime_id INT, type VARCHAR(255), episodes INT, rating DECIMAL, members INT);" +
+    "LOAD DATA LOCAL INFILE 'csv/animeTable.csv' INTO TABLE ANIME FIELDS TERMINATED BY ',' IGNORE 1 LINES (name, anime_id, type, episodes, rating, members);"
     executeQuery(query,res)
 })
 
 app.get('/resetAnimeGenre', (req,res) => {
-  const query = "DROP TABLE IF EXISTS Anime_Genre;"
-  "CREATE TABLE Anime_Genre (row_id INT AUTO_INCREMENT PRIMARY KEY, anime_id INT, genre_id INT);" +
-    "LOAD DATA LOCAL INFILE 'csv/animeGenreTable.csv' INTO TABLE Anime_Genre FIELDS TERMINATED BY ',' IGNORE 1 LINES (anime_id, genre_id);"
+  const query = "DROP TABLE IF EXISTS GENRES;" 
+  + "CREATE TABLE GENRES (genre VARCHAR(255), id INT);"
+  +"LOAD DATA LOCAL INFILE 'csv/genresTable.csv' INTO TABLE GENRES FIELDS TERMINATED BY ',' IGNORE 1 LINES;"
     executeQuery(query,res)
 })
 
+app.get('/resetOtherAnimeGenreTable', (req,res) => {
+  const query = "DROP TABLE IF EXISTS ANIME_GENRE;"
+  + "CREATE TABLE ANIME_GENRE (row_id INT AUTO_INCREMENT PRIMARY KEY, anime_id INT, genre_id INT);"
+  + "LOAD DATA LOCAL INFILE 'csv/animeGenreTable.csv' INTO TABLE ANIME_GENRE FIELDS TERMINATED BY ',' IGNORE 1 LINES (anime_id, genre_id);"
+    executeQuery(query,res)
+})
+
+
  app.get('/resetUsers',(req,res) => {
-    const query = 'DROP TABLE IF EXISTS Users;'
+    const query = 'DROP TABLE IF EXISTS USERS;'
     + "CREATE TABLE USERS (user_id INT, first_name VARCHAR(255), last_name VARCHAR(255), email VARCHAR(255), gender VARCHAR(255), username TEXT, "
     + "password VARCHAR(255), avatar_image VARCHAR(255), hex_color VARCHAR(255));"
     + "LOAD DATA LOCAL INFILE 'csv/usersTable.csv' INTO TABLE USERS FIELDS TERMINATED BY ',' IGNORE 1 LINES "
@@ -58,17 +66,17 @@ app.get('/resetAnimeGenre', (req,res) => {
  })
 
  app.get('/resetReviews',(req,res) => {
-  const query = 'DROP TABLE IF EXISTS Reviews;'
-    + "CREATE TABLE Reviews (row_id INT AUTO_INCREMENT PRIMARY KEY, review VARCHAR(255), anime_id INT, user_id INT, date_time DATETIME);"
-    + "LOAD DATA LOCAL INFILE 'csv/reviewsTable.csv' INTO TABLE Reviews FIELDS TERMINATED BY ',' "
+  const query = 'DROP TABLE IF EXISTS REVIEWS;'
+    + "CREATE TABLE REVIEWS (row_id INT AUTO_INCREMENT PRIMARY KEY, review VARCHAR(255), anime_id INT, user_id INT, date_time DATETIME);"
+    + "LOAD DATA LOCAL INFILE 'csv/reviewsTable.csv' INTO TABLE REVIEWS FIELDS TERMINATED BY ',' "
     + "IGNORE 1 LINES (review, anime_id, user_id, date_time);"
   executeQuery(query,res)
 })
 
 app.get('/resetUserReviews',(req,res) => {
-  const query = 'DROP TABLE IF EXISTS User_Review;'
-    + "CREATE TABLE User_Review (user_id INT, review VARCHAR(255));"
-    + "LOAD DATA LOCAL INFILE 'csv/userReviewTable.csv' INTO TABLE User_Review FIELDS TERMINATED BY ',' "
+  const query = 'DROP TABLE IF EXISTS USER_REVIEW;'
+    + "CREATE TABLE USER_REVIEW (user_id INT, review VARCHAR(255));"
+    + "LOAD DATA LOCAL INFILE 'csv/userReviewTable.csv' INTO TABLE USER_REVIEW FIELDS TERMINATED BY ',' "
     + "IGNORE LINES 1 (user_id, review);" 
   
   executeQuery(query,res)
@@ -80,7 +88,7 @@ app.get('/allGenres', (req,res) => {
 })
 
 app.get('/largestAnimeId',(req,res) => {
-  const query = "SELECT MAX(anime_id) AS largest_anime_id FROM Anime"
+  const query = "SELECT MAX(anime_id) AS largest_anime_id FROM ANIME"
   executeQuery(query,res)
 })
 
